8f755c5890e142b1ca862fff929666a2871f1019
ElmarJ <elmar@elmarjansen.nl>
2017-05-26 10:53:04 +0200

    Database triggers for "active" property updated, so it now works when ignoring active children.

--- functions/index.js
+++ functions/index.js
@@ -3,30 +3,37 @@
 var functions = require('firebase-functions');
 
 // if done changes, update "active"
-exports.donechange = functions.database.ref('/users/{uid}/hubbles/{hubblekey}/done/{done}').onWrite(event => {
+exports.donechange = functions.database.ref('/users/{uid}/hubbles/{hubblekey}/done').onWrite(event => {
     return updateactive(event.data.ref.parent);
 });
 
 // if snooze changes, update "active"
-exports.snoozedchange = functions.database.ref('/users/{uid}/hubbles/{hubblekey}/snoozed/{snoozed}').onWrite(event => {
+exports.snoozedchange = functions.database.ref('/users/{uid}/hubbles/{hubblekey}/snoozed').onWrite(event => {
     return updateactive(event.data.ref.parent);
 });
 
 // if active child count changes, update "active"
-exports.activecountchange = functions.database.ref('/users/{uid}/hubbles/{hubblekey}/activechildcount/{activecount}').onWrite(event => {
+exports.activecountchange = functions.database.ref('/users/{uid}/hubbles/{hubblekey}/activechildcount').onWrite(event => {
     return updateactive(event.data.ref.parent);
 });
 
 // if active changes, update "active-child-count of parent"
-exports.activechange = functions.database.ref('/users/{uid}/hubbles/{hubblekey}/active/{active}').onWrite(event => {
-    const parentKeyRef = event.data.ref.child('parent').ref;
-    parentKeyRef.once('value').then(parentkey => updateActiveChildCount(functions.database.ref('/users/{uid}/hubbles/' + parentkey)))
+exports.activechange = functions.database.ref('/users/{uid}/hubbles/{hubblekey}/active').onWrite(event => {
+    if (false) {
+        const parentKeyRef = event.data.ref.child('parent').ref;
+        parentKeyRef.once('value').then(snapshot => {
+            const hubble_key = snapshot.val();
+            updateActiveChildCount(functions.database.ref('/users/{uid}/hubbles/' + hubble_key));
+        });
+    }
 });
 
 // if parent changes, update "active-child-count of parent"
-exports.parentchange = functions.database.ref('/users/{uid}/hubbles/{hubblekey}/parent/{parentkey}').onWrite(event => {
-    event.data.ref.once('value').then(parentkey => updateActiveChildCount(functions.database.ref('/users/{uid}/hubbles/' + parentkey)));
-    event.data.previous.ref.once('value').then(parentkey => updateActiveChildCount(functions.database.ref('/users/{uid}/hubbles/' + parentkey)));
+exports.parentchange = functions.database.ref('/users/{uid}/hubbles/{hubblekey}/parent').onWrite(event => {
+    if (false) {
+        event.data.ref.once('value').then(snapshot => updateActiveChildCount(functions.database.ref('/users/{uid}/hubbles/' + snapshot.val())));
+        event.data.previous.ref.once('value').then(snapshot => updateActiveChildCount(functions.database.ref('/users/{uid}/hubbles/' + snapshot.val())));
+    }
 });
 
 /**
@@ -35,9 +42,11 @@ exports.parentchange = functions.database.ref('/users/{uid}/hubbles/{hubblekey}/
  * @param {admin.database.Reference} hubbleRef 
  */
 function updateactive(hubbleRef) {
-    return hubbleRef.once(
-        hubble => { return hubbleRef.child('active').set(isactive(hubble.snoozed, hubble.done, hubble.activechildcount)) }
-    );
+    return hubbleRef.once('value').then(
+        snapshot => {
+            const hubble = snapshot.val();
+            return hubbleRef.child('active').set(isactive(hubble.snoozed, hubble.done, 0));
+        });
 }
 
 /**
@@ -48,8 +57,8 @@ function updateactive(hubbleRef) {
  * @param {number} activechilds 
  * @returns 
  */
-function isactive(snoozed, done, activechilds) {
-    return !snoozed && (!done || (activechilds > 0))
+function isactive(snoozed, done, activechildren) {
+    return !snoozed && (!done || (activechildren > 0))
 }
 
 /**
@@ -59,7 +68,8 @@ function isactive(snoozed, done, activechilds) {
  */
 function updateActiveChildCount(hubbleRef) {
 
-    return hubbleRef.child('key').once(parentKey => {
+    return hubbleRef.child('key').once('value').then(snapshot => {
+        const parentKey = snapshot.val();
         return query = hubbleRef.parent.orderByChild('parent').equalTo(parentKey).orderByChild('active').equalTo(true);
     }).then(query => {
         return query.once('value').then(
