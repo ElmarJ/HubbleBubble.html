f41315b460fed91eff4d1de4c80a8f8fda3527f4
ElmarJ <elmar@elmarjansen.nl>
2018-01-16 16:01:29 +0100

    Complete rewrite of functions, to a much simpler structure (thanks to firestore migration) and using TypeScript

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,111 +3,56 @@ import { database } from "firebase-admin";
 
 admin.initializeApp(functions.config().firebase);
 
-// if done changes, update "active"
-const donechange = functions.database
-  .ref("/users/{uid}/hubbles/{hubblekey}/done")
-  .onWrite(event => {
-    return updateActivity(event.data.ref.parent);
+// Update the "active" property
+const updateMyActivity = functions.firestore
+  .document("users/{userId}/hubbles/{hubbleId}")
+  .onUpdate(async event => {
+    const newValue = event.data.data();
+
+    const computedActivity = isActive(
+      newValue.snoozed,
+      newValue.done,
+      newValue.activechildren
+    );
+
+    // Only update if the current value is incorrect
+    if (newValue.active !== computedActivity) {
+      await event.data.ref.set({
+        active: computedActivity
+      });
+    }
   });
 
-// if snooze changes, update "active"
-const snoozedchange = functions.database
-  .ref("/users/{uid}/hubbles/{hubblekey}/snoozed")
-  .onWrite(event => updateActivity(event.data.ref.parent));
+const updateParentActiveChildCount = functions.firestore
+  .document("users/{userId}/hubbles/{hubbleId}")
+  .onUpdate(async event => {
+    const newValue = event.data.data();
+    const oldValue = event.data.previous.data();
 
-// if number of active children changes, update "active"
-const activechildrenchange = functions.database
-  .ref("/users/{uid}/hubbles/{hubblekey}/activechildren")
-  .onWrite(event => updateActivity(event.data.ref.parent));
+    // If neither activity nor parent changed, nothing is to be updated
+    if (
+      newValue.active === oldValue.active &&
+      newValue.parent === oldValue.parent
+    )
+      return;
 
-// if activity changes, update "active children count" of parent
-const activechange = functions.database
-  .ref("/users/{uid}/hubbles/{hubblekey}/active")
-  .onWrite(event =>
-    updateActivityCountParent(event.data.ref.parent, event.data.val())
-  );
+    // Not super efficient, but very straight forward:
+    //    first reduce old parent's activity (if I was active)
 
-// if child show up, set me as its parent.
-const childrenlistentrychange = functions.database
-  .ref("/users/{uid}/childrenof/{parentkey}/{childkey}")
-  .onWrite(event => {
-    // 1. set parentkey of child hubble
-    // 2. sanitation: remove all other childrenof entries with same child key (if present)
-    const childKey = event.data.val();
-    const thisHubbleRef = event.data.ref.parent.parent;
-
-    if (childKey) {
-      const childsParentKeyRef = thisHubbleRef.parent
-        .child(childKey)
-        .child("parent");
-      return childsParentKeyRef.set(thisHubbleRef.key);
+    if (oldValue.active) {
+      const parentRef = oldValue.parent;
+      const parent = (await parentRef.ref.get()).data();
+      parentRef.set({ activechildren: parent.activechildren-- });
     }
 
-    return null;
-  });
-
-const childrenlistchange = functions.database
-  .ref("/users/{uid}/childrenof/{parentkey}")
-  .onWrite(async event => {
-    // 1. recount active children of parent hubble.
-
-    // a complete recount - even when children have only been sorted - is not the most efficient way to do this
-    // Solution: move this logic to parent-change, and substract one from old parent + add one to new parent if
-    // hubble is active.
-
-    const children = event.data.val();
-    const activeChildCountRef = event.data.ref.parent.child("activechildren");
-
-    const promisesToGetChildActivity = [];
-
-    for (const childkey in children) {
-      const ref = event.data.ref.parent.parent.child(childkey).child("active");
-      promisesToGetChildActivity.push(async () =>
-        (await ref.once("value")).val()
-      );
+    //    then increase new parent's activity (if I am active)
+    if (newValue.active) {
+      const parentRef = newValue.parent;
+      const parent = (await parentRef.ref.get()).data();
+      parentRef.set({ activechildren: parent.activechildren++ });
     }
-
-    const childActivities = await Promise.all(promisesToGetChildActivity);
-
-    const activeChildCount = childActivities.reduce((counter, activity) => {
-      if (activity) return counter + 1;
-      return counter;
-    }, 0);
-
-    activeChildCountRef.set(activeChildCount);
   });
-
-async function updateActivity(hubbleRef: database.Reference) {
-  const snapshot = await hubbleRef.once("value");
-  const hubble = snapshot.val();
-  const activity = isActive(hubble.snoozed, hubble.done, hubble.activechildren);
-  hubbleRef.child("active").set(activity);
-}
 
 function isActive(snoozed: boolean, done: boolean, activechildren: number) {
   return !snoozed && (!done || activechildren > 0);
-}
-
-function updateActivityCountParent(
-  hubbleRef: database.Reference,
-  newValue: boolean
-) {
-  return getParentRef(hubbleRef).then(parentRef => {
-    return parentRef
-      .child("activechildren")
-      .once("value")
-      .then(childCountSnapshot => {
-        let count = childCountSnapshot.val();
-        if (newValue) count++;
-        else count--;
-        if (count < 0) count = 0;
-        parentRef.child("activechildren").set(count);
-      });
-  });
-}
-
-async function getParentRef(hubbleRef: database.Reference) {
-  const parentSnapshot = await hubbleRef.child("parent").once("value");
-  const key = parentSnapshot.val();
-  return hubbleRef.parent.child(key);
 }
