5dae896ca4cd2dad5c5f70a7ae37a2b689023489
ElmarJ <elmar@elmarjansen.nl>
2018-03-02 16:40:36 +0100

    Solve duplicate firebase-intialization bug

--- public/src/index.ts
+++ public/src/index.ts
@@ -11,15 +11,6 @@ window.onhashchange = function() {
 };
 
 window.addEventListener("load", () => {
-  firebase.initializeApp({
-    "apiKey": "AIzaSyA7zawVhVUXXzD5XKVSMBkqunjEI-Mp5Ek",
-    "databaseURL": "https://hubblebubble-58ec9.firebaseio.com",
-    "storageBucket": "hubblebubble-58ec9.appspot.com",
-    "authDomain": "hubblebubble-58ec9.firebaseapp.com",
-    "messagingSenderId": "889012243145",
-    "projectId": "hubblebubble-58ec9"
-  });
-
   // Load / save view-settings for this user:
   document.documentElement.setAttribute(
     "class",
