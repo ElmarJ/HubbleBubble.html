efd2b80fdd4cc7b0176a5056e33713edce47f61f
Elmar Jansen <elmar@elmarjansen.nl>
2019-01-13 11:10:39 +0100

    Add setting to suppress warning about new timestamp behaviour firebase

--- public/src/init.ts
+++ public/src/init.ts
@@ -1,10 +1,13 @@
 
-if (typeof firebase === 'undefined') { throw new Error('hosting/init-error: Firebase SDK not detected. You must include it before /__/firebase/init.js'); }
 firebase.initializeApp({
-  "apiKey": "AIzaSyA7zawVhVUXXzD5XKVSMBkqunjEI-Mp5Ek",
-  "databaseURL": "https://hubblebubble-58ec9.firebaseio.com",
-  "storageBucket": "hubblebubble-58ec9.appspot.com",
-  "authDomain": "hubblebubble-58ec9.firebaseapp.com",
-  "messagingSenderId": "889012243145",
-  "projectId": "hubblebubble-58ec9"
+  apiKey: "AIzaSyA7zawVhVUXXzD5XKVSMBkqunjEI-Mp5Ek",
+  databaseURL: "https://hubblebubble-58ec9.firebaseio.com",
+  storageBucket: "hubblebubble-58ec9.appspot.com",
+  authDomain: "hubblebubble-58ec9.firebaseapp.com",
+  messagingSenderId: "889012243145",
+  projectId: "hubblebubble-58ec9"
 });
+
+const firestore = firebase.firestore();
+const settings = {timestampsInSnapshots: true};
+firestore.settings(settings);
