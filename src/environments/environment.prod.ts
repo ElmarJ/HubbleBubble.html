60c98b73bda91c0b60c16d7204c8a165d924ff61
Elmar Jansen <elmar@elmarjansen.nl>
2019-11-01 16:43:39 +0100

    Add firebase settings to production evnironment settings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyA7zawVhVUXXzD5XKVSMBkqunjEI-Mp5Ek',
+    authDomain: 'hubblebubble-58ec9.firebaseapp.com',
+    databaseURL: 'https://hubblebubble-58ec9.firebaseio.com',
+    storageBucket: 'hubblebubble-58ec9.appspot.com',
+    messagingSenderId: '889012243145',
+    projectId: 'hubblebubble-58ec9'
+  }
 };
